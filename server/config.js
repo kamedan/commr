e7380156d931d779b8bb1b472b6a782ec05a6526
DESKTOP-BS34OR0\LENOVO <informatique@blachere-factory.com>
2018-03-31 12:18:39 +0100

    setup node server and angular homepage with data services

---
+++ server/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    database: 'mongodb://amin:amin@ds229909.mlab.com:29909/amazono123',
+    port: 3000,
+    secret :'aminesecret'
+}
---
+++ server/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const config = require('./config');
+
+const app = express();
+
+mongoose.connect(config.database, (err) => {
+    if(err){
+        console.log(err)
+    }else{
+        console.log('connected to database');
+    }
+})
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(morgan('dev'));
+
+app.use(cors());
+
+
+app.get('/', (req, res, next) => {
+    res.json({
+        user: 'amine'
+    })
+})
+
+app.listen(config.port, (err)=> {
+    console.log('server running');
+})
